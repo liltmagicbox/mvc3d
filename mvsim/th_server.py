0d3d328fad65ac1f5bd694692089152c0806db82
liltmagicbox <68154458+liltmagicbox@users.noreply.github.com>
2022-09-07 13:33:32 +0900

    putgetseemswork
    transfering

--- mvsim/th_server.py
+++ mvsim/th_server.py
@@ -1,6 +1,5 @@
 import socket
 
-
 class Server:
     def __init__(self, host='localhost', port=30030):        
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -11,7 +10,25 @@ class Server:
         self.conn = conn
     
     def run(self):
-        1
+        while True:
+            strdata = self.conn.recv(16).decode()
+            #print(strdata)
+            
+            if strdata == 'get':
+                strdata = 'fulldataall'
+
+                blen = str(len(strdata)).encode()
+                self.conn.sendall(blen)
+                self.conn.sendall(strdata.encode())
+                #self.conn.send(f"{dlen}".encode())
+            elif strdata == 'put':
+                blen = int( self.conn.recv(16).decode() )
+                print(blen)            
+                strdata = self.conn.recv(blen).decode()
+                print(strdata)            
+
+                        
+
     def put(self,data):
         1
     def get(self):
@@ -19,18 +36,20 @@ class Server:
         if strdata == 'key':
             keys = '[1,2,3]'.encode()
             self.conn.sendall(keys)
-        elif strdata == 'view':
-            strdata = self.conn.recv(1024).decode()
-        
-        return strdata
+        elif 'view' in strdata:
+            print(strdata)
+            strdata
+            buffersize=2
+            data=[]
+            while True:
+                chunk = self.conn.recv(buffersize)
+                print(chunk)
+                data.append(chunk)                
+            fulldata = b''.join(data)
+            return fulldata
 
 s = Server()
-
-while True:
-    data = s.get()
-    if not data:
-        break
-    print(data)
+s.run()
 
 exit()
 
